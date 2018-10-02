6ce89e869ffff3773db74d69d1180ede0cae3e4d
horace <hp264@cornell.edu>
2018-10-02 12:52:15 -0700

    fix face rot bugs

--- cli.py
+++ cli.py
@@ -1,18 +1,22 @@
 from cube import Cube, ACTIONS
+import argparse
+import time
 import random
 import os
+import pdb
 
 def clear_screen():
     os.system('clear')
 
 def parse_user_cmd(s):
     '''
-    Allowed format is: 'x.y.z.{etc}', where x, y, z are in ACTIONS
+    s: command string
+       Allowed format is: 'x.y.z.{etc}', where x, y, z are in ACTIONS
+    Returns: list of allowed moves(u/d/l/r/f/b)
     '''
     moves = s.split('.')
     for m in moves:
-        assert (m in ACTIONS or m == 'clear')
-
+        assert (m in ACTIONS or m == 'clear' or m == 'pdb')
     return moves
 
 def interpreter(cube):
@@ -24,12 +28,13 @@ def interpreter(cube):
 
         if 'clear' in moves:
             cube.reset()
+        elif 'pdb' in moves:
+            pdb.set_trace()
         else:
             cube.step(moves)
-        cube.render()
 
 def random_run():
-    cube = Cube(3)
+    cube = Cube(2)
     moves = ['u', 'd', 'l', 'r', 'f', 'b']
     for _ in range(100):
         move = random.choice(moves)
@@ -38,9 +43,16 @@ def random_run():
         time.sleep(0.1)
         print('-' * 80)
 
+def get_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--size', type=int, default=3)
+    return parser.parse_args()
+
 def main():
-    cube = Cube(3)
+    config = get_config()
+    cube = Cube(config.size)
     interpreter(cube)
 
 if __name__ == '__main__':
-    main() 
+    #random_run()
+    main()
--- cube.py
+++ cube.py
@@ -43,17 +43,6 @@ class Cube:
         self.f = np.array([['W' for _ in range(size)] for _ in range(size)])
         self.b = np.array([['Y' for _ in range(size)] for _ in range(size)])
 
-        # make them all 3x3 for now
-        self.tiles = {f: np.array([[0, 1, 2], [3, 4,5], [6, 7, 8]]) for f in FACES}
-        self.faces = {
-            'u': self.u,
-            'd': self.d,
-            'l': self.l,
-            'r': self.r,
-            'f': self.f,
-            'b': self.b,
-        }
-
     def reset(self):
         self.u = np.array([['G' for _ in range(self.size)] for _ in range(self.size)])
         self.d = np.array([['B' for _ in range(self.size)] for _ in range(self.size)])
@@ -61,19 +50,6 @@ class Cube:
         self.r = np.array([['M' for _ in range(self.size)] for _ in range(self.size)])
         self.f = np.array([['W' for _ in range(self.size)] for _ in range(self.size)])
         self.b = np.array([['Y' for _ in range(self.size)] for _ in range(self.size)])
-        self.faces = {
-            'u': self.u,
-            'd': self.d,
-            'l': self.l,
-            'r': self.r,
-            'f': self.f,
-            'b': self.b,
-        }
-
-        if self.size == 3:
-            self.tiles = np.array([[0, 1, 2], [3, 4,5], [6, 7, 8]])
-        elif self.size == 4:
-            self.tiles = np.array([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]])
 
     def render(self, _ascii=False):
         if _ascii:
@@ -109,6 +85,9 @@ class Cube:
                 self.render_face()
                 sys.stdout.write('\n')
 
+    def get_face(self, face_orientation):
+        pass
+
     def render_face(self, face_orientation=None, row=0):
         # renders a single row of the given face
         # we render row by row b/c we need to render the row of a whole bunch of faces
@@ -117,48 +96,43 @@ class Cube:
             for i in range(self.size):
                 sys.stdout.write(BLANKTILE)
         else:
-            _face = self.faces[face_orientation]
+            _face = getattr(self, face_orientation)
             for i in range(self.size):
                 color = _face[row][i]
-                face_tiles = self.tiles[face_orientation]
-                tile = ' {:2s}'.format(str(face_tiles[row, i]))
                 coloredtile = colored(TILE, COLORDICT[color], attrs=['reverse'])
-                #coloredtile = colored(tile, COLORDICT[color], attrs=['reverse'])
                 sys.stdout.write(coloredtile)
 
     def rot_u(self):
-        print('rot u new')
-        new_colors = [
+        new_colors = (
             tuple(self.r[TOP, :]),
             tuple(self.f[TOP, :]),
             tuple(self.l[TOP, :]),
             tuple(self.b[TOP, :])
-        ]
+        )
         self.f[TOP, :] = new_colors[0]
         self.l[TOP, :] = new_colors[1]
         self.b[TOP, :] = new_colors[2]
-        pdb.set_trace()
         self.r[TOP, :] = new_colors[3]
 
     def rot_d(self):
-        new_colors = [
+        new_colors = (
             tuple(self.r[BOT, :]),
             tuple(self.f[BOT, :]),
             tuple(self.l[BOT, :]),
             tuple(self.b[BOT, :])
-        ]
+        )
         self.f[BOT, :] = new_colors[0]
         self.l[BOT, :] = new_colors[1]
         self.b[BOT, :] = new_colors[2]
         self.r[BOT, :] = new_colors[3]
 
     def rot_r(self):
-        new_colors = [
+        new_colors = (
             tuple(self.d[:, RIGHT]),
             tuple(self.f[:, RIGHT]),
             tuple(reversed(self.u[:, RIGHT])),
             tuple(reversed(self.b[:, LEFT]))
-        ]
+        )
         # thats just the colors!
         self.f[:, RIGHT] = new_colors[0]
         self.u[:, RIGHT] = new_colors[1]
@@ -166,46 +140,77 @@ class Cube:
         self.d[:, RIGHT] = new_colors[3]
 
     def rot_l(self):
-        new_colors = [
+        new_colors = (
             tuple(self.d[:, LEFT]),
             tuple(self.f[:, LEFT]),
             tuple(reversed(self.u[:, LEFT])),
             tuple(reversed(self.b[:, RIGHT]))
-        ]
+        )
         self.f[:, LEFT] = new_colors[0]
         self.u[:, LEFT] = new_colors[1]
         self.b[:, RIGHT]  = new_colors[2]
         self.d[:, LEFT] = new_colors[3]
 
+    def rot_f(self):
+        new_colors = (
+            tuple(self.l[:, RIGHT]),
+            tuple(self.u[BOT, :]),
+            tuple(reversed(self.r[:, LEFT])),
+            tuple(self.d[TOP, :])
+        )
+        self.u[BOT, :] = new_colors[0]
+        self.r[:, LEFT] = new_colors[1]
+        self.d[TOP, :]  = new_colors[2]
+        self.l[:, RIGHT] = new_colors[3]
+
+    def rot_b(self):
+        new_colors = (
+            tuple(self.r[:, RIGHT]),
+            tuple(reversed(self.u[TOP, :])),
+            tuple(self.l[:, LEFT]),
+            tuple(reversed(self.d[BOT, :]))
+        )
+        self.u[TOP, :] = new_colors[0]
+        self.l[:, LEFT] = new_colors[1]
+        self.d[BOT, :]  = new_colors[2]
+        self.r[:, RIGHT] = new_colors[3]
+
+    def get_face(self, face_char):
+        if face_char == 'u':
+            return self.u
+        elif face_char == 'd':
+            return self.d
+        elif face_char == 'l':
+            return self.l
+        elif face_char == 'r':
+            return self.r
+        elif face_char == 'f':
+            return self.f
+        elif face_char == 'b':
+            return self.b
+        else:
+            raise ValueError('Not a valid face orientation')
+
     def rotate(self, face):
         # TODO: should probably avoid doing the copy
         # TODO: inverse moves
-        _face = self.faces[face]
-        self.faces[face] = np.rot90(_face, axes=(1,0))
-        self.tiles[face] = np.rot90(self.tiles[face], axes=(1,0))
+        # TODO: using setattr is a little gross...
+        _face = getattr(self, face)
+        rotated_face = np.rot90(_face, axes=(1,0)) # this needs to act on self.u/d/l/r/f/b
+        setattr(self, face, rotated_face)
 
         if face is 'u':
-            # rotate top stuff
-            #self.l[TOP,:], self.b[TOP,:], self.r[TOP,:], self.f[TOP,:] = self.f[TOP, :].copy(), self.l[TOP,:].copy(), self.b[TOP,:].copy(), self.r[TOP,:].copy()
             self.rot_u()
         elif face is 'd':
-            # left bot, front bot, right bot, back bot
-            #self.l[BOT,:], self.f[BOT,:], self.r[BOT,:], self.b[BOT,:] = self.b[BOT, :].copy(), self.l[BOT,:].copy(), self.f[BOT,:].copy(), self.r[BOT,:].copy()
             self.rot_d()
         elif face is 'l':
-            # front left -> down left -> back right -> up left
-            #self.f[:,LEFT], self.d[:,LEFT], self.b[:,RIGHT], self.u[:,LEFT] = self.u[:,LEFT].copy(), self.f[:,LEFT].copy(), self.d[:,LEFT].copy(), self.b[:,RIGHT].copy()
             self.rot_l()
         elif face is 'r':
-            # front right -> up right -> back left -> down right
-            #self.f[:,RIGHT], self.u[:,RIGHT], self.b[:,LEFT], self.d[:,RIGHT] = self.d[:,RIGHT].copy(), self.f[:,RIGHT].copy(), self.u[:,RIGHT].copy(), self.b[:,LEFT].copy()
             self.rot_r()
         elif face is 'f':
-            # up bot -> right left -> down top -> left right
-            self.r[:,LEFT], self.d[TOP,:], self.l[:,RIGHT], self.u[BOT,:] = self.u[BOT,:].copy(), self.r[:,LEFT].copy(), self.d[TOP,:].copy(), self.l[:,RIGHT].copy()
+            self.rot_f()
         elif face is 'b':
-            # left left -> down bot -> right right -> up top
-            self.l[:,LEFT], self.d[BOT,:], self.r[:,RIGHT], self.u[TOP,:] = self.u[TOP, :].copy(), self.l[:,LEFT].copy(), self.d[BOT,:].copy(), self.r[:,RIGHT].copy()
+            self.rot_b()
 
 
     def step(self, moves):
